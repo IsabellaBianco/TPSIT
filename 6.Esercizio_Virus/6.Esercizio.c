ae6bfed1d5070da6e48484f1d56677bd540c2740
IsabellaBianco <71838030+IsabellaBianco@users.noreply.github.com>
2020-10-30 16:44:26 +0100

    Update 6.Esercizio.c

--- 6.Esercizio_Virus/6.Esercizio.c
+++ 6.Esercizio_Virus/6.Esercizio.c
@@ -8,18 +8,18 @@ int main(){
     FILE* fin;
 
     char car;
+    //Apro il file e verifico la corretta apertura
     if((fin = fopen("rna.txt", "r"))== NULL){
         printf("Errore nell'apertura del file!\n");
     }
-
+    //Dichiaro ed inizializzo le variabili
     int a = 0, c = 0, g = 0, t = 0;
     int n;
 
 
     while((car = fgetc(fin))!= EOF){
 
-        
-
+        //Leggo un carattere e nello switch guardo se il carattere acquisito è uguale ad a, c, g o t
             switch(car){
 
                 case 'a':
